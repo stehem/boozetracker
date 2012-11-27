6e5e8fe683a27747840739440968f75774f985d8
stephane <stephane@stephane-W54xEU.(none)>
2012-11-27 19:46:20 +0100

    hreoku and mongo and clojure = no joy

--- src/boozetracker/db.clj
+++ src/boozetracker/db.clj
@@ -2,8 +2,24 @@
  (:use [somnium.congomongo]))
 
 
+(defn split-mongo-url [url]
+  "Parses mongodb url from heroku, eg. mongodb://user:pass@localhost:1234/db"
+  (let [matcher (re-matcher #"^.*://(.*?):(.*?)@(.*?):(\d+)/(.*)$" url)] ;; Setup the regex.
+    (when (.find matcher) ;; Check if it matches.
+      (zipmap [:match :user :pass :host :port :db] (re-groups matcher))))) ;; Construct an options map.
+
+
 (def conn
-  (make-connection "beertabs"
-                   :host "127.0.0.1"
-                   :port 27017))
+  (let [mongo-url (get (System/getenv) "MONGOHQ_URL")]
+    (if mongo-url
+      (let [config (split-mongo-url mongo-url)]
+        (authenticate
+          (make-connection  (:db config)
+                            (:host config)
+                            (Integer. (:port config)))
+          (:user config) (:pass config)))
+        (make-connection "beertabs"
+                          :host "127.0.0.1"
+                          :port 27017) )))
+
 
