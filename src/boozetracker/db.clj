5ab4f520c0acc8f42f2f3fa232756943dd3bf3f9
stephane <stephane@stephane-W54xEU.(none)>
2012-12-01 11:19:53 +0100

    OK this will work or else

--- src/boozetracker/db.clj
+++ src/boozetracker/db.clj
@@ -10,11 +10,15 @@
 
 (defn conn []
 "Checks if connection and collection exist, otherwise initialize."
-  (when (not (connection? *mongo-config*)) ;; If global connection doesn't exist yet.
-    (let [mongo-url (get (System/getenv) "MONGOHQ_URL") ;; Heroku puts it here.
-config (split-mongo-url mongo-url)] ;; Extract options.
+    (let [mongo-url (get (System/getenv) "MONGOHQ_URL")] ;; Heroku puts it here.
+      (if mongo-url
+    (let [config (split-mongo-url mongo-url)] ;; Extract options.
       (println "Initializing mongo @ " mongo-url)
       (mongo! :db (:db config) :host (:host config) :port (Integer. (:port config))) ;; Setup global mongo.
       (authenticate (:user config) (:pass config)) ;; Setup u/p.
       (or (collection-exists? :firstcollection) ;; Create collection named 'firstcollection' if it doesn't exist.
-(create-collection! :firstcollection)))))
+(create-collection! :firstcollection)))
+      (make-connection "beertabs" "127.0.0.1" 27017)
+    )
+  )
+  )
