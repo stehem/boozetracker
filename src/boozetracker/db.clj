c0a4c60d501d26ee75b281460cd693bbcf239702
stephane <stephane@stephane-W54xEU.(none)>
2012-12-01 11:37:48 +0100

    OK this will work or else2

--- src/boozetracker/db.clj
+++ src/boozetracker/db.clj
@@ -3,19 +3,14 @@
   (:use [somnium.congomongo.config :only [*mongo-config*]]))
 
 (defn split-mongo-url [url]
-  "Parses mongodb url from heroku, eg. mongodb://user:pass@localhost:1234/db"
-  (let [matcher (re-matcher #"^.*://(.*?):(.*?)@(.*?):(\d+)/(.*)$" url)] ;; Setup the regex.
-    (when (.find matcher) ;; Check if it matches.
-      (zipmap [:match :user :pass :host :port :db] (re-groups matcher))))) ;; Construct an options map.
+  (let [matcher (re-matcher #"^.*://(.*?):(.*?)@(.*?):(\d+)/(.*)$" url)]
+    (when (.find matcher)
+      (zipmap [:match :user :pass :host :port :db] (re-groups matcher)))))
 
 (defn conn []
-"Checks if connection and collection exist, otherwise initialize."
-    (let [mongo-url (get (System/getenv) "MONGOHQ_URL")] ;; Heroku puts it here.
-      (if mongo-url
-    (let [config (split-mongo-url mongo-url)] ;; Extract options.
-      (set-connection! (make-connection mongo-url))
-      )
-      (set-connection! (make-connection "beertabs" "127.0.0.1" 27017))
-    )
-  )
-  )
+  (let [mongo-url (get (System/getenv) "MONGOHQ_URL")]
+    (if mongo-url
+      (let [config (split-mongo-url mongo-url)]
+        (mongo! :db (:db config) :host (:host config) :port (Integer. (:port config)))
+        (authenticate (:user config) (:pass config))  )
+      (set-connection! (make-connection "beertabs" "127.0.0.1" 27017))  ) ) )
