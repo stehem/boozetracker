59441bdc6768dcb01c35a301cd5d05ca61155fdc
stephane <stephane@stephane-W54xEU.(none)>
2013-01-14 23:32:23 +0100

    this never gets old

--- src/boozetracker/db.clj
+++ src/boozetracker/db.clj
@@ -2,9 +2,17 @@
 
 (use 'korma.db)
 
+(def db-url
+  (or (System/getenv "DATABASE_URL") "postgres://postgres:jajapostgres@localhost:5432/boozetracker"))
 
-(defdb db
-    (postgres {:db "boozetracker"
-                :user "uozmlvsjcwxysb"
-                :host "0emljmQhei5ugjdF73bbbmZpxK"
-                :password "0emljmQhei5ugjdF73bbbmZpxK"}))
+(defn split-db-url [url]
+  "Parses database url from heroku, eg. postgres://user:pass@localhost:1234/db"
+  (let [matcher (re-matcher #"^.*://(.*?):(.*?)@(.*?):(\d+)/(.*)$" url)] ;; Setup the regex.
+    (when (.find matcher) ;; Check if it matches.
+      (zipmap [:match :user :password :host :port :db] (re-groups matcher))))) ;; Construct an options map.
+
+
+(defdb dev (postgres (split-db-url db-url)))
+
+
+
