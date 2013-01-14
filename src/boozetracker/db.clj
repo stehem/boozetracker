a7f065fca12381b39e8d5f824787d46c5d1a6b8e
stephane <stephane@stephane-W54xEU.(none)>
2013-01-14 23:04:46 +0100

    this never gets old

--- src/boozetracker/db.clj
+++ src/boozetracker/db.clj
@@ -4,16 +4,7 @@
 
 
 (defdb db
-  (if (System/getenv "DATABASE_URL")
-    (let [db-uri (java.net.URI. (System/getenv "DATABASE_URL"))
-          user-and-password (clojure.string/split (.getUserInfo db-uri) #":")]
-      {:classname "org.postgresql.Driver"
-        :subprotocol "postgresql"
-        :user (get user-and-password 0)
-        :password (get user-and-password 1) ; may be nil
-        :subname (if (= -1 (.getPort db-uri))
-          (format "//%s%s" (.getHost db-uri) (.getPath db-uri))
-          (format "//%s:%s%s" (.getHost db-uri) (.getPort db-uri) (.getPath db-uri)))})
     (postgres {:db "boozetracker"
-                :user "postgres"
-                :password "jajapostgres"})))
+                :user "uozmlvsjcwxysb"
+                :host "0emljmQhei5ugjdF73bbbmZpxK"
+                :password "0emljmQhei5ugjdF73bbbmZpxK"}))
